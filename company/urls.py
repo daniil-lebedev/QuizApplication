baade546f223fd02df02c0127672875d1049057c
Daniil Lebedev <d.lebedev2@newcastle.ac.uk>
2024-03-26 18:33:06 +0000

    feat: Allows to edit team

--- company/urls.py
+++ company/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import views
-from .views import TeamDetailView
+from .views import TeamDetailView, edit_team
 
 urlpatterns = [
     path("", views.index, name="index"),
@@ -11,4 +11,6 @@ urlpatterns = [
     path("show_all_teams", views.show_all_teams, name="show_all_teams"),
     path("team_detail/<int:team_id>", login_required(TeamDetailView.as_view()), name="team_detail"),
     path("manage_team_view", views.manage_team_view, name="manage_team_view"),
+    path("join_team/<int:team_id>", views.join_team, name="join_team"),
+    path('team/<int:team_id>/edit/', edit_team, name='edit_team'),
 ]
